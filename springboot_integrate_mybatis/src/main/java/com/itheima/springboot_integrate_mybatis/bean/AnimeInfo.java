a456742f3a148d8eb74a2cbf41368972c709c84f
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-25 18:31:25 +0800

    AM

---
+++ springboot_integrate_mybatis/src/main/java/com/itheima/springboot_integrate_mybatis/bean/AnimeInfo.java
@@ -0,0 +1,13 @@
+package com.itheima.springboot_integrate_mybatis.bean;
+
+/**
+ * Description==>TODO
+ * BelongsProject==>springboot_pom
+ * BelongsPackage==>com.itheima.springboot_integrate_mybatis.bean
+ * CreateTime==>2022-09-25 18:11:13
+ * Version==>1.0
+ * Author==>02雪乃赤瞳楪祈校条祭制作委员会 wyq_start
+ */
+public class AnimeInfo {
+
+}
---
+++ springboot_integrate_mybatis/src/main/java/com/itheima/springboot_integrate_mybatis/dao/AnimeInfo.java
@@ -0,0 +1,16 @@
+package com.itheima.springboot_integrate_mybatis.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * Description==>TODO
+ * BelongsProject==>springboot_pom
+ * BelongsPackage==>com.itheima.springboot_integrate_mybatis.dao
+ * CreateTime==>2022-09-25 18:08:43
+ * Version==>1.0
+ * Author==>02雪乃赤瞳楪祈校条祭制作委员会 wyq_start
+ */
+@Mapper
+public interface AnimeInfo {
+
+}
