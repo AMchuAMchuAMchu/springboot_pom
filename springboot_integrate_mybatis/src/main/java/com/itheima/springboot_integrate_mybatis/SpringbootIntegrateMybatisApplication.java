e3b55b5d1bd1d1219a386b55d51615c931ece51c
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-25 17:59:35 +0800

    AM

---
+++ springboot_integrate_mybatis/src/main/java/com/itheima/springboot_integrate_mybatis/SpringbootIntegrateMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.itheima.springboot_integrate_mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIntegrateMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootIntegrateMybatisApplication.class, args);
+    }
+
+}
---
+++ springboot_integrate_mybatis/src/test/java/com/itheima/springboot_integrate_mybatis/SpringbootIntegrateMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.springboot_integrate_mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIntegrateMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
