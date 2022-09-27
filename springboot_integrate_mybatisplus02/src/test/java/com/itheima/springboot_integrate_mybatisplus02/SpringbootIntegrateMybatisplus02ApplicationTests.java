af05bc28e02d0c88391792d9c48e723579d21b61
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-27 16:39:28 +0800

    AM

--- springboot_integrate_mybatisplus02/src/test/java/com/itheima/springboot_integrate_mybatisplus02/SpringbootIntegrateMybatisplus02ApplicationTests.java
+++ springboot_integrate_mybatisplus02/src/test/java/com/itheima/springboot_integrate_mybatisplus02/SpringbootIntegrateMybatisplus02ApplicationTests.java
@@ -1,5 +1,8 @@
 package com.itheima.springboot_integrate_mybatisplus02;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.springboot_integrate_mybatisplus02.dao.AnimeService;
 import com.itheima.springboot_integrate_mybatisplus02.pojo.AnimeInfo;
 import org.junit.jupiter.api.Test;
@@ -13,6 +16,30 @@ class SpringbootIntegrateMybatisplus02ApplicationTests {
 
     @Autowired
     private AnimeService animeService;
+
+
+
+
+    @Test
+    void testWrapperQuery(){
+
+        Page<AnimeInfo> animeInfoPage = new Page<>(1, 2);
+        LambdaQueryWrapper<AnimeInfo> ailqw = new LambdaQueryWrapper<>();
+        ailqw.like(AnimeInfo::getName,"之");
+        Page<AnimeInfo> animeInfoPage1 = animeService.selectPage(animeInfoPage, ailqw);
+        animeInfoPage1.getRecords().forEach(System.out::println);
+    }
+
+
+    @Test
+    void testPageOne(){
+
+        Page<AnimeInfo> animeInfoPage = new Page<>(2, 3);
+        Page<AnimeInfo> animeInfoPage1 = animeService.selectPage(animeInfoPage, null);
+        animeInfoPage1.getRecords().forEach(System.out::println);
+
+    }
+
 
     @Test
     void testSelectAllAnime(){
