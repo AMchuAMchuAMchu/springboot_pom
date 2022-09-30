f7a8f2db685cf9fcd7af2ad9c1c2549208579b76
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-30 18:47:35 +0800

    AM

--- springboot_integrate_ssm/src/main/java/com/itheima/springboot_integrate_mybatisplus02/SpringbootIntegrateMybatisplus02Application.java
+++ springboot_integrate_ssm/src/main/java/com/itheima/springboot_integrate_mybatisplus02/SpringbootIntegrateMybatisplus02Application.java
@@ -8,6 +8,7 @@ public class SpringbootIntegrateMybatisplus02Application {
 
     public static void main(String[] args) {
 
+
         SpringApplication.run(SpringbootIntegrateMybatisplus02Application.class, args);
     }
 
